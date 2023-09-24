ce8af0346d6f6675dd191b56397f3b40687f366c
Fayaz-21 <fayaztunio06@gmail.com>
2023-09-24 14:49:35 +0500

    first commit

---
+++ Hello.js
@@ -0,0 +1,11 @@
+console.log("This is a beginning of a new project")
+
+const add=(a)=>(b)=>a+b
+const addition=add(10)(20)
+//console.log(addition)
+
+const greeting=(name='Fayaz')=>{
+    console.log(`Hello ${name} and the sum is ${addition}`)
+}
+
+greeting('Hassan')
